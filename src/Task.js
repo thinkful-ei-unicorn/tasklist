8408d49238db7cafec93449fab7ab3300659177f
Rhonda Bell <rhonda.bell@thinkful.com>
2020-05-31 14:04:37 -0500

    Tasklist and Task

---
+++ src/Task.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Task(props) {
+    return (
+        <div style={{textDecoration: props.complete ? 'line-through' : null,}}
+            className="task"
+            onClick={() => {
+                // How do I call completeTask in <TaskList />?
+            }}>
+            {props.text}
+        </div>
+    );
+}
+
+export default Task;
---
+++ src/Tasklist.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Task from './Task';
+
+class TaskList extends Component {
+    state = {
+        tasks: [
+            {id: 0, text: 'Learn React', complete: false},
+            {id: 1, text: '???', complete: false},
+            {id: 2, text: 'Profit', complete: false}
+        ]
+    };
+
+    completeTask = (id) => { // Note: Arrow function
+        // This needs calling when a <Task /> is clicked
+        this.setState({
+            tasks: this.state.tasks.map(
+                task => task.id === id ? {...task, complete: true} : task
+            )
+        });
+    };
+
+    render() {
+        return(
+           <>
+            
+           </>
+        )
+    }
+}
+
+export default TaskList;
